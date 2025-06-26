e864b4d9bc15d45b5bdbb8f70fc9c29892e662c1
Mahan Mazaheri <mahan78ir@gmail.com>
2025-06-26 11:11:09 +0330

    add search feature to app

--- apiApp/views.py
+++ apiApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from .models import Cart, CartItem, Product ,Category, Review , WishList
@@ -171,3 +172,19 @@ def add_to_wishlist(request):
     new_wishlist = WishList.objects.create(user=user, product=product)
     serializer = WishListSerializer(new_wishlist)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_search(request):
+    query = request.query_params.get("query")
+    if not query:
+        return Response("No Query Provided", status=400)
+    
+    products = Product.objects.filter(Q(name__icontains=query) | 
+                                      Q(description__icontains=query) |
+                                      Q(category__name__icontains=query))
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
+    if not products.exists():
+        return Response({"error": "No products found"}, status=404)
+    
