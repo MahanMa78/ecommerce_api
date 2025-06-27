3ac8992e15d4cfd6baa46e75c971a57a55e6f80c
Mahan Mazaheri <mahan78ir@gmail.com>
2025-06-27 15:04:03 +0330

    add Product tests

--- apiApp/tests.py
+++ apiApp/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from .models import CustomUser , Product , Category
+
+class ProductTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create(
+             email="testuser@email.com" 
+        )
+        
+        cls.category = Category.objects.create(
+            name = "Sport car"
+        )
+        
+        
+        cls.product = Product.objects.create(
+            name = "Car 1" , 
+            description = "good car" , 
+            price = 120 ,
+            category = cls.category
+        )    
+
+    def test_category_model_name(self):
+        self.assertEqual(self.category.name , "Sport car")
+    
+    def test_product_model_name(self):
+        self.assertEqual(self.product.name ,"Car 1" )
+        
+    def test_product_model_description(self):
+        self.assertEqual(self.product.description , "good car")
+        
+    def test_product_model_price(self):
+        self.assertEqual(self.product.price , 120)
+     
+    def test_product_model_description(self):
+        self.assertEqual(self.product.category.name, "Sport car")
+        
+        
+    
